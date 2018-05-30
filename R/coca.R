a94ccf013467668372b5fec4a9babc1635826bd9
Alessandra Cabassi <ac2051@cam.ac.uk>
2018-05-30 12:19:39 +0100

    change output of coca() function

--- R/coca.R
+++ R/coca.R
@@ -9,7 +9,7 @@
 #' @param pItem proportion of items sampled at each iteration of the Consensus Cluster step.
 #' @param hclustMethod method to be used by the hclust function for the hierarchical clustering step.
 #' @return The output is a consensus matrix, that is a symmetric matrix where the element in position (i,j) corresponds to
-#' the proportion of times that items i and j have been clustered together.
+#' the proportion of times that items i and j have been clustered together and a vector of cluster labels.
 #' @author Alessandra Cabassi \email{ac2051@cam.ac.uk}
 #' @references Monti, S., Tamayo, P., Mesirov, J. and Golub, T., 2003. Consensus clustering: a resampling-based method for
 #' class discovery and visualization of gene expression microarray data. Machine learning, 52(1-2), pp.91-118.
@@ -19,15 +19,17 @@
 
 coca = function(dataset, K, B = 1000, pItem = 0.8, hclustMethod = 'average'){
 
-  # # Step 1. Build matrix of clusters
-  # do.call(rbind,matrix.list)
+
+  output = list()
 
   # Step 1. Compute the consensus matrix
-  consensusMatrix <- consensusCluster(dataset, K, B, pItem)
+  output$consensusMatrix <- consensusCluster(dataset, K, B, pItem)
 
   # Step 2. Use hierarchical clustering on the consensus matrix to find the clustering
-  distances <- as.dist(1-consensusMatrix)
+  distances <- as.dist(1 - output$consensusMatrix)
   hClustering <- hclust(distances, method = hclustMethod)
-  clusterLabels <- cutree(hClustering, K)
+  output$clusterLabels <- cutree(hClustering, K)
+
+  return(output)
 
 }
